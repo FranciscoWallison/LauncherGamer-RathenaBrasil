fc1d902d350a4d659547e75d5b4617975cbcd588
Francisco Wallison <franciscowallison@gmail.com>
2021-04-16 12:17:38 -0300

    add codigo fonte electron js

---
+++ electron-js/README.md
@@ -0,0 +1,27 @@
+# Launcher Gamer-RathenaBrasil
+
+### OBS
+    Os arquivos de configuração na rais deste projeto são necessários arquivos.
+    Tendo como base o arquivo .env.example
+    pode renomear para .env, com as demais informações como "LOGO="logo.png" e "IMAGE_BACKGROUND="background.jpg".
+
+
+## Pre-requisitos de instalação
+[Node.js](https://nodejs.org/en/)
+
+[NPM](https://www.npmjs.com/)
+
+[ELECTRON.JS](https://www.electronjs.org/)
+
+
+## Comandos 
+    - npm install ou yarn install
+    - npm run dev ou yarn dev
+    - npm run dist ou yarn dist
+
+O comando ````npm install ou yarn install```` para baixar todos os pacotes.
+O comando ````npm run dev ou yarn dev```` executando o projeto.
+O comando ````npm run dist ou yarn dist```` builder projeto.
+
+
+## Esse projeto foi desenvolvido com as seguintes tecnologias:
---
+++ electron-js/index.js
@@ -0,0 +1,30 @@
+const {
+    app, 
+    BrowserWindow
+} = require('electron');
+require('dotenv').config()
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        // width: process.env.WIDTH,
+        // height:process.env.HEIGHT,
+        // fullscreen: process.env.FULLSCREEN,
+        width: 900,
+        height:700,
+        fullscreen: false,
+        resizable: false,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            devTools: false
+        }
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    let dir = __dirname.replace("\\resources", "").replace("\\app.asar", "");
+    mainWindow.setIcon(`${dir}\\`+process.env.FILE_ICON);
+
+    // shell.openItem(app.getAppPath() + '\\local\\print.bat');
+})
